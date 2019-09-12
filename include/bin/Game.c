ec5c23f0a51d678611a6c5b130d0796eb4ed8dae
stiroy <55147197+stiroy@users.noreply.github.com>
2019-09-12 23:36:31 +0200

    ny eventhandleing working progress

--- include/bin/Game.c
+++ include/bin/Game.c
@@ -34,19 +34,41 @@ void HandleEvents(Game* g){
         if(g->event.type == SDL_QUIT){
                 *g->noExit = 0;
         }
+
         g->player.x_dir = 0;
         g->player.y_dir = 0;
-        const Uint8 *keyboardState = SDL_GetKeyboardState(NULL);
-        if (keyboardState[SDL_SCANCODE_LEFT] || keyboardState[SDL_SCANCODE_A]) {
-            g->player.x_dir -= 1;
+        
+        //behöver översättas
+        /*if(g->event.type = SDL_KEYDOWN){
+            printf("Physical %s key acting as %s key\n",
+                    SDL_GetScancodeName(g->event.key.keysym.scancode),
+                    SDL_GetKeyName(g->event.key.keysym.sym));
         }
-        if (keyboardState[SDL_SCANCODE_RIGHT] || keyboardState[SDL_SCANCODE_D]) {
-            g->player.x_dir += 1;
-        }
-        if (keyboardState[SDL_SCANCODE_UP] || keyboardState[SDL_SCANCODE_W]) {
+
+        /*if(g->event.key.keysym.sym == SDLK_w){
             g->player.y_dir -= 1;
         }
-        if (keyboardState[SDL_SCANCODE_DOWN] || keyboardState[SDL_SCANCODE_S]) {
+        if(g->event.key.keysym.sym == SDLK_a){
+            g->player.x_dir -= 1;
+        }
+        if(g->event.key.keysym.sym == SDLK_s){
+            g->player.y_dir += 1;
+        }
+        if(g->event.key.keysym.sym == SDLK_d){
+            g->player.x_dir += 1;
+        }*/
+        const Uint8 *Keys = SDL_GetKeyboardState(NULL);
+        SDL_PumpEvents();
+        if (Keys[SDL_SCANCODE_LEFT] || Keys[SDL_SCANCODE_A]) {
+            g->player.x_dir -= 1;
+        }
+        if (Keys[SDL_SCANCODE_RIGHT] || Keys[SDL_SCANCODE_D]) {
+            g->player.x_dir += 1;
+        }
+        if (Keys[SDL_SCANCODE_UP] || Keys[SDL_SCANCODE_W]) {
+            g->player.y_dir -= 1;
+        }
+        if (Keys[SDL_SCANCODE_DOWN] || Keys[SDL_SCANCODE_S]) {
             g->player.y_dir += 1;
         }
     }   
