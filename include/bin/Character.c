4cc1e9d9c27efb4a0aea302a74b6e9f1c6161860
Mats-Hjalmar Murman <matte.hj1234@gmail.com>
2019-09-12 14:57:21 +0200

    Character + Hitbox

--- include/bin/Character.c
+++ include/bin/Character.c
@@ -1,42 +1,12 @@
 #include "../SDL2/SDL.h"
 #include "TileMap.h"
-/*
+
 int
 MoveCharacter(int x, int y, char moveDir)
 {
-    switch(moveDir)
-    {
-        case 'a':
-            if (x > 0)
-            {
-                x--;
-                return 1;
-            }
-            break;
-        case 'd':
-            if (x < TILES_X)
-            {
-                x++;
-                return 1;
-            }
-            break;
-        case 'w':
-            if (y > 0)
-            {
-                y--;
-                return 1;
-            }
-            break;
-        case 's':
-            if (x < TILES_Y)
-            {
-                y++;
-                return 1;
-            }
-            break;
-    }
+    
     return 0;
-}*/
+}
 
 int AnimateCharacter(int x, int y, char dir)
 {
