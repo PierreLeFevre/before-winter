30b9d42fcd89b6ae5ca5d2f81ca11093a6ccc243
stiroy <55147197+stiroy@users.noreply.github.com>
2019-09-12 14:52:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,12 @@
 
 int main()
 {
+    SDL_Event event;
     Game g;
     ConstructGame(&g);
     int noExit = 1;
-    SDL_SetRenderDrawColor(g.gfx.rend,0,0,0,255); //Ritar skärmen grön varje frame för att sedan få banan utritad
-        SDL_RenderClear(g.gfx.rend);
     while(noExit){
         Go(&g);
-        SDL_Event event;
         while(SDL_PollEvent(&event)){
             switch(event.type){
             case SDL_QUIT:
