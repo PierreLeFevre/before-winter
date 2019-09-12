c1966d529a67fba5c313a5fcab394f4a962b1ed5
Pierre <pierrelfevre@gmail.com>
2019-09-12 11:16:01 +0200

    merge

--- main.c
+++ main.c
@@ -5,13 +5,8 @@ int main()
     Game g;
     ConstructGame(&g);
     int noExit = 1;
-<<<<<<< HEAD
     SDL_SetRenderDrawColor(g.gfx.rend,0,255,0,255); //Ritar skärmen grön varje frame för att sedan få banan utritad
         SDL_RenderClear(g.gfx.rend);
-=======
-    SDL_SetRenderDrawColor(g.gfx.rend,0,255,0,0); //Ritar skärmen grön varje frame för att sedan få banan utritad
-    SDL_RenderClear(g.gfx.rend);
->>>>>>> e96ad8769c5bbe88e021ca3da881a17852cb5bf5
     while(noExit){
         Go(&g);
         SDL_Event event;
