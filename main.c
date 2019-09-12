e96ad8769c5bbe88e021ca3da881a17852cb5bf5
Mats-Hjalmar Murman <matte.hj1234@gmail.com>
2019-09-12 11:14:14 +0200

    heltal

--- main.c
+++ main.c
@@ -5,11 +5,9 @@ int main()
     Game g;
     ConstructGame(&g);
     int noExit = 1;
-    SDL_SetRenderDrawColor(g.gfx.rend,0,255,0,0.2); //Ritar skärmen grön varje frame för att sedan få banan utritad
-        SDL_RenderClear(g.gfx.rend);
+    SDL_SetRenderDrawColor(g.gfx.rend,0,255,0,0); //Ritar skärmen grön varje frame för att sedan få banan utritad
+    SDL_RenderClear(g.gfx.rend);
     while(noExit){
-        
-
         Go(&g);
         SDL_Event event;
         while(SDL_PollEvent(&event)){
