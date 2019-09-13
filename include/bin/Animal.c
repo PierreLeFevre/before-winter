dbaf968f8d1e54839fe957ccdc81ad9a2fb2a8c2
rackaracka123 <matte.hj1234@gmail.com>
2019-09-13 18:36:55 +0200

    Mini Update

--- include/bin/Animal.c
+++ include/bin/Animal.c
@@ -37,28 +37,22 @@ void UpdateAnimalDirection(Animal* animal){
 
 
     Ai(animal);
-
     //down
     if (animal->d.srcrect.y > animal->Border.h){
         animal->d.srcrect.y = animal->Border.h;
-        printf("1");
     }
     //up
     if(animal->d.srcrect.y < animal->Border.y){
         animal->d.srcrect.y = animal->Border.y;
-        printf("2");
     }
     //right
     if(animal->d.srcrect.x > animal->Border.w){
         animal->d.srcrect.x = animal->Border.w;
-        printf("3");
     }
     //left
     if(animal->d.srcrect.x < animal->Border.x){
         animal->d.srcrect.x = animal->Border.x;
-        printf("4");
     }
-    
 }
 
 void MoveAnimal(Animal* animal){    
@@ -66,46 +60,51 @@ void MoveAnimal(Animal* animal){
     animal->d.srcrect.y += animal->y_vel * animal->y_dir;
 }
 
-<<<<<<< HEAD
 void DrawAnimal(Animal* animal){
     animal->d.srcrect.y -= animal->d.srcrect.h - TILE_HEIGHT;
     animal->d.srcrect.y += animal->d.srcrect.h - TILE_HEIGHT;
 }
 
-=======
->>>>>>> d72727d536f6e67b5d5dd7cc0ac8128ed1686031
 void AnimateAnimal(Animal* animal){
-    if(animal->x_dir == 1){
+    while(animal->x_dir != 0 && animal->y_dir != 0)
+    {
+        MoveAnimal(animal);
+        if(animal->x_dir > 0){animal->x_dir -=1;}
+        if(animal->x_dir < 0){animal->x_dir +=1;}
 
+        if(animal->y_dir > 0){animal->y_dir -=1;}
+        if(animal->y_dir < 0){animal->y_dir +=1;}
     }
 }
 int Ai(Animal *a)
 {
     a->aiIterations++;
-
-    a->x_dir = 30;
-            a->y_dir = 30;
     if (a->aiTarget == a->aiIterations)
     {
-        int r = rand() % 100;
-        if (r >= 70)
+        a->aiTarget = rand() % 100;
+        a->aiIterations = 0;
+
+        if (a->aiTarget >= 70)
         {
             //moo
         }
-        if (r > 60 && r < 70)
+        if (a->aiTarget > 20 && a->aiTarget < 70)
         {
             //Walk
             
-
+            a->x_dir = rand() % (50 + 1 - -2) + -2;
+            a->y_dir = rand() % (50 + 1 - -2) + -2;
+            return 1;
         }
-        if (r > 30 && r < 50)
+        if (a->aiTarget > 30 && a->aiTarget < 50)
         {
             //shit
         }
-        if (r > 0 && r < 30)
+        if (a->aiTarget > 0 && a->aiTarget < 30)
         {
             //eat
         }
-        a->aiIterations = 0;
+        
+        
     }
 }
