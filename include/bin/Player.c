68b868ef3f383094d3e6b291be90cd952b62a7e0
Emil Karlsson <personal.emil.karlsson@outlook.com>
2019-09-13 08:59:11 +0200

    Player stuck to tiles

--- include/bin/Player.c
+++ include/bin/Player.c
@@ -25,33 +25,45 @@ void UpdatePlayer(Player* player){
 }
 
 void UpdatePlayerDirection(Player* player){
-
-    player->x_dir = 0;
-    player->y_dir = 0;
-    const Uint8 *Keys = SDL_GetKeyboardState(NULL);
-    SDL_PumpEvents();
-    if (Keys[SDL_SCANCODE_LEFT] || Keys[SDL_SCANCODE_A]) {
-        player->x_dir -= 1;
-    }
-    if (Keys[SDL_SCANCODE_RIGHT] || Keys[SDL_SCANCODE_D]) {
-        player->x_dir += 1;
-    }
-    if (Keys[SDL_SCANCODE_UP] || Keys[SDL_SCANCODE_W]) {
-        player->y_dir -= 1;
-    }
-    if (Keys[SDL_SCANCODE_DOWN] || Keys[SDL_SCANCODE_S]) {
-        player->y_dir += 1;
+    if(player->x_pos_destCoord == player->x_pos_currCoord &&
+    player->y_pos_destCoord == player->y_pos_currCoord)
+    {
+        player->x_dir = 0;
+        player->y_dir = 0;
+        const Uint8 *Keys = SDL_GetKeyboardState(NULL);
+        SDL_PumpEvents();
+        if (Keys[SDL_SCANCODE_LEFT] || Keys[SDL_SCANCODE_A]) {
+            player->x_dir -= 1;
+        }
+        if (Keys[SDL_SCANCODE_RIGHT] || Keys[SDL_SCANCODE_D]) {
+            player->x_dir += 1;
+        }
+        if (Keys[SDL_SCANCODE_UP] || Keys[SDL_SCANCODE_W]) {
+            player->y_dir -= 1;
+        }
+        if (Keys[SDL_SCANCODE_DOWN] || Keys[SDL_SCANCODE_S]) {
+            player->y_dir += 1;
+        }
+        player->x_pos_destCoord = player->x_pos_currCoord + player->x_dir;
+        player->y_pos_destCoord = player->y_pos_currCoord + player->y_dir;
     }
 }
 
-void MovePlayer(Player* player){
-    
+void MovePlayer(Player* player){    
     player->d.srcrect.x += player->x_vel * player->x_dir;
     player->d.srcrect.y += player->y_vel * player->y_dir;
+    if(player->d.srcrect.x / TILE_WIDTH == player->x_pos_destCoord){
+        player->x_pos_currCoord = player->x_pos_destCoord;
+    }
+    if(player->d.srcrect.y / TILE_HEIGHT == player->y_pos_destCoord){
+        player->y_pos_currCoord = player->y_pos_destCoord;
+    }
 }
 
 void DrawPlayer(Player* player){
+    player->d.srcrect.y -= player->d.srcrect.h - TILE_HEIGHT;
     Draw(&player->d);
+    player->d.srcrect.y += player->d.srcrect.h - TILE_HEIGHT;
 }
 
 void AnimatePlayer(Player* player){
