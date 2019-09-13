5b14e9d1e475dcc405846807dbcbedd4e1762dc5
stiroy <55147197+stiroy@users.noreply.github.com>
2019-09-13 10:23:20 +0200

    SOLID 60FPS

--- main.c
+++ main.c
@@ -2,11 +2,24 @@
 #include <stdio.h>
 int main()
 {
+    const int FPS = 60;
+    const int Framedelay = 1000 / FPS;
+
+    Uint32 frameStart;
+    int frameTime;
+
     int noExit = 1;
     Game g;    
     ConstructGame(&g, &noExit);
     while(noExit){
-        Go(&g);
+        frameStart = SDL_GetTicks();
+        Go(&g);  //init()
+        frameTime = SDL_GetTicks() - frameStart;
+        if (Framedelay > frameTime)
+        {
+            SDL_Delay(Framedelay - frameTime);
+        }
+        
     }
     DestroyGame(&g);
     return 0;
