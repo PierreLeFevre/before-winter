a494b193c36c0e2dce6f97cd981e7a34f5e0e3f3
Emil Karlsson <personal.emil.karlsson@outlook.com>
2019-09-16 11:52:44 +0200

    fix

--- include/bin/Game.c
+++ include/bin/Game.c
@@ -7,7 +7,6 @@ void ConstructGame(Game *g, int* noExit){
     ConstructGraphics(&g->gfx);
     ConstructTileMap(&g->tileMap, &g->gfx, 30, 30, 0, 0, "./TileMap.txt");
     ConstructPlayer(&g->player, &g->gfx);
-    ConstructEntity(&g->entity[0], &g->gfx);
     ConstructCamera(&g->cam, &g->gfx, &g->player.d.srcrect);
 
     g->noExit = noExit;
@@ -26,13 +25,11 @@ void Go(Game *g){
 void UpdateLogic(Game *g){
     HandleEvents(g);
     UpdatePlayer(&g->player);
-    UpdateEntity(&g->entity[0]);
     UpdateCamera(&g->cam);
 }
 void Render(Game *g){ 
     int nToRender = 0;
     AddTileMapToRenderList(&g->tileMap, &g->cam, g->RenderList, &nToRender);
-    AddToRenderList(g, &g->entity[0].d, &nToRender);
     AddToRenderList(g, &g->player.d, &nToRender);
     
     SortRenderList(g, &nToRender);
