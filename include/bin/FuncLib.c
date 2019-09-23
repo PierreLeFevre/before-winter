27ecde46665f0f89b7128ca09df26cd13104cd6d
rackaracka123 <55146597+rackaracka123@users.noreply.github.com>
2019-09-23 12:28:54 +0200

    peiren

--- include/bin/FuncLib.c
+++ include/bin/FuncLib.c
@@ -125,14 +125,11 @@ char* IntToCharArray(int number)
 
     int n = log10(number) + 1;
     int k;
-    char *array = calloc(n, sizeof(char));
+    char *array = SDL_calloc(n, sizeof(char));
     for ( k = 0; k < n; ++k, number /= 10 )
     {
         array[k] = number % 10;
     }
-
-    
-
     if (isNegative){
         array[k] = '-';
     }
