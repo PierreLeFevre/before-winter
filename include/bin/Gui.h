e9898df614a26706704cf97b5ca06e1854e4e40f
Pierre <pierrelfevre@gmail.com>
2019-09-25 10:56:59 +0200

    message box

--- include/bin/Gui.h
+++ include/bin/Gui.h
@@ -8,13 +8,18 @@
 typedef struct Gui{
     Drawable d;
     Drawable charToPrint;
+    Drawable messageBox;
+    char message[101];
+    int messageActive;
     Player* p;
 
 }Gui;
 
 void ConstructGui(Gui* g, Graphics* gfx, Player* p);
 void UpdateGui(Gui* g);
-void DrawGuiBoxes(Gui* g);
 void RenderText(Gui* g, int x, int y, int b, char text[]);
+
+void MsgBoxShow(Gui* g, int messageActive);
+void MsgBoxText(Gui* g, char message[101]);
 
 #endif
