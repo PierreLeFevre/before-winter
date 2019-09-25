0fc8d3a0a5ad4c3c5419792b7fb5caa1561fd112
Emil Karlsson <personal.emil.karlsson@outlook.com>
2019-09-25 10:40:37 +0200

    Added Distance function

--- include/bin/FuncLib.h
+++ include/bin/FuncLib.h
@@ -4,10 +4,9 @@
 #include "../SDL2/SDL_rect.h"
 
 void RemoveCharacterFromArray(char* const buffer, char toRemove, int size);
-
 double Map(double value, double range_1_min, double range_1_max, double range_2_min, double range_2_max);
-
 int Cap(int value_in, int cap_to);
+float Dist(float x1, float y1, float x2, float y2);
 
 int CheckCollision(SDL_Rect A, SDL_Rect B);//lazy foo http://lazyfoo.net/tutorials/SDL/27_collision_detection/index.php
 int sign(int A);
