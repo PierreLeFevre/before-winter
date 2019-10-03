00b846b5bcec4593c2b5f40b2c9bd93f592839d2
Pierre <pierrelfevre@gmail.com>
2019-10-03 15:44:08 +0200

    fixed player srcrect size

--- include/bin/Graphics.c
+++ include/bin/Graphics.c
@@ -48,7 +48,7 @@ void ConstructGraphics(Graphics *gfx)
     SDL_FreeSurface(surf);
 
     //PLAYER
-    surf = IMG_Load("include/assets/unpacked/Characters/haley.png");
+    surf = IMG_Load("include/assets/unpacked/Characters/robin.png");
     if (!surf)
     {
         printf("Error creating surface (Leah.png): %s\n", SDL_GetError());
